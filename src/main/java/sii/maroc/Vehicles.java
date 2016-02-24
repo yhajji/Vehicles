432a1d4975230a4d1ad2367c88283c27d4f44dc7
Younes HAJJI <y.hajji@sii-maroc.com>
2016-02-24 18:25:15 +0100

    create branch

---
+++ src/main/java/sii/maroc/Vehicles.java
@@ -0,0 +1,14 @@
+package sii.maroc;
+
+public class Vehicles {
+
+    public Vehicles(String string) {
+	// TODO Auto-generated constructor stub
+    }
+
+    public String move(String string, String string2, String string3, String string4) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
